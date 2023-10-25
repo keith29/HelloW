ad426c9efcbcf330c649d2d5e2ca8ce3a1cc2aff
Keith Williams <keith299@gmail.com>
2023-10-25 14:34:59 -0700

    Changed what is printed out.

--- src/com/kwilliams/hello/HelloWorld.java
+++ src/com/kwilliams/hello/HelloWorld.java
@@ -2,6 +2,6 @@ package com.kwilliams.hello;
 
 public class HelloWorld {
     public static void main(String[] args){
-        System.out.println("Hello");
+        System.out.println("Well, well, what a change");
     }
 }
